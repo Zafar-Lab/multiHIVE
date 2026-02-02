9d747227a3b90db5af6e5c6f7da12a50ec509331
Krushna Pavan <krushnapavan9@gmail.com>
2026-02-02 17:36:39 +0530

    Add documentation and tutorials

---
+++ docs/source/conf.py
@@ -0,0 +1,43 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../../src/"))
+
+project = 'multiHIVE'
+copyright = '2026, krushna'
+author = 'krushna'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinxcontrib.bibtex",
+    "nbsphinx",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.duration",
+    "sphinx.ext.napoleon",
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.mathjax',
+]
+intersphinx_mapping = {
+    "scvi-tools": ("https://docs.scvi-tools.org/en/1.3.2/", None),
+}
+bibtex_bibfiles = ["references.bib"]
+templates_path = ["_templates"]
+exclude_patterns = []
+
+nbsphinx_execute = 'never'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_book_theme"
+html_static_path = ["_static"]
